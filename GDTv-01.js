ebd14cd4ec7cc8563b10459237331b21e30d9e5d
gianellis-cafe <167910101+gianellis-cafe@users.noreply.github.com>
2024-04-23 14:21:04 -0400

    Add files via upload

---
+++ GDTv-01.js
@@ -0,0 +1,23 @@
+
+
+//////////////////////// Section 1 User Interface  ////////////////////////
+
+//Navbar
+
+function openNav() {
+  document.getElementById("hamburgerStack").style.display = "block";
+
+}
+function closeNav() {
+  document.getElementById("hamburgerStack").style.display = "";
+}
+
+//Order Cluster
+
+function openOrderStack() {
+  document.getElementById("order-overlay").style.display = "block";
+
+}
+function closeOrderStack() {
+  document.getElementById("order-overlay").style.display = "";
+}
